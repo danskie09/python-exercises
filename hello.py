767708a149047964885e6d800318c8071fedf81e
danskie09 <banaybanaydaniel09@gmail.com>
2025-06-10 19:08:36 +0800

    Age Calculation after 100 years

---
+++ hello.py
@@ -0,0 +1,15 @@
+print("Enter your name:")
+name = input()
+
+print("Enter your age:")
+age = int(input())
+
+
+currentYear = 2025
+
+calculate = currentYear - age + 100
+
+
+
+print(f"Hello this is your age{calculate}")
+
