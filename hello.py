431bc97f9bf82262665b79acf620c3fad35d109d
danskie09 <banaybanaydaniel09@gmail.com>
2025-06-12 19:07:58 +0800

    divisors with list

--- hello.py
+++ hello.py
@@ -1,17 +1,13 @@
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+num = int(input("Enter a number"))
+
+divisors = []
 
 
 
-num = int(input("input a number:   "))
-
-
-filtered = [i for i in a if i < num]
-
-newList = list(set(filtered))
-
-print(newList)
+for i in range(1,num + 1):
+    if num % i == 0:
+        divisors.append(i)
 
 
 
-
-
+print(divisors)
