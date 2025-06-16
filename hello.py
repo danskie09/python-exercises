14afe7b2406d6fa47d03e6ac336a66e9c990376d
danskie09 <banaybanaydaniel09@gmail.com>
2025-06-16 21:06:16 +0800

    Rock Paper Scissors GAme

--- hello.py
+++ hello.py
@@ -1,38 +1,35 @@
+rock = 1
+paper = 2
+scissors = 3
 
+p1score = 0
+p2score = 0
 
-import random
+gameOver = 3
 
+print("Rock = 1, Paper = 2, Scissors = 3")
 
+while True:
+    player1 = int(input("Player 1 - Enter your choice: "))
+    player2 = int(input("Player 2 - Enter your choice: "))
 
-a = random.sample(range(1,101), 12)
+    if player1 == player2:
+        print("It's a tie!")
+    elif (player1 == rock and player2 == scissors) or \
+         (player1 == paper and player2 == rock) or \
+         (player1 == scissors and player2 == paper):
+        p1score += 1
+        if p1score == gameOver:
+            print("Player 1 is the Champion")
 
-b = random.sample(range(1,101), 12)
-
-
-print(a)
-print(b)
-
-
-
-
-
-mergeList = a + b
-
-
-
-seen = set()
-
-duplicates = set()
-
-for items in mergeList:
-    if items in seen:
-        duplicates.add(items)
+        print("Player 1 wins!" + str(p1score))
     else:
-        seen.add(items)
 
+        p2score += 1
+        if p2score == gameOver:
+            print("Player 2 is the champion")
+        print("Player 2 wins! " + str(p2score))
 
-
-print(list(seen))
-
-
-
+    command = input("Do you want to exit? (Yes/No): ")
+    if command.lower() == "yes":
+        break
