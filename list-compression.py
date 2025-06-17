e50316f3ee3cfef826f228b54a96118f6611e62a
danskie09 <banaybanaydaniel09@gmail.com>
2025-06-17 19:06:43 +0800

    list compression, find odd product

--- list-compression.py
+++ list-compression.py
@@ -2,11 +2,19 @@ x = [1, 2, 3]
 y = [5, 10, 15]
 
 allProducts = []
+oddProducts = []
 
 for a in x:
     for b in y:
         allProducts.append(a*b)
+        product = a*b
+        if product % 2 != 0:
+            oddProducts.append(product)
+
+
+
 
 
 
 print(allProducts)
+print(oddProducts)
