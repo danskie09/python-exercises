040b343761b207d6953b95e01e7578412636327a
Daniel Banaybanay <banaybanaydaniel09@gmail.com>
2025-08-25 18:34:31 +0800

    ph level exercise

---
+++ codedex/ph_levels.py
@@ -0,0 +1,17 @@
+
+
+
+run = 0;
+
+
+while(run == 0):
+    ph = int(input("Please enter the PH Value:"))
+    if ph > 7:
+        print("Basic")
+    elif ph < 7:
+        print("Acidic")
+    else:
+        print("Neutral")
+        
+        
+        
