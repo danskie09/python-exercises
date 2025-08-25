c593ecdd008a5678cf0dcc29c87126c25bfb1319
Daniel Banaybanay <banaybanaydaniel09@gmail.com>
2025-08-25 18:44:36 +0800

    magic8

---
+++ codedex/magic8.py
@@ -0,0 +1,30 @@
+import random
+
+run = 1
+while run == 1:
+    question = input("Ask the Magic 8-Ball a question (or type 'quit' to exit): ")
+    
+    if question.lower() == "quit":
+        run = 0
+        break
+    
+    number = random.randint(1, 9)
+    
+    if number == 1:
+        print("Yes - definitely.")
+    elif number == 2:
+        print("It is decidedly so.")
+    elif number == 3:
+        print("Without a doubt.")
+    elif number == 4:
+        print("Reply hazy, try again.")
+    elif number == 5:
+        print("Ask again later.")
+    elif number == 6:
+        print("Better not tell you now.")
+    elif number == 7:
+        print("My sources say no.")
+    elif number == 8:
+        print("Outlook not so good.")
+    elif number == 9:
+        print("Very doubtful.")
